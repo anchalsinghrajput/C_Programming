5fd520fec8101d9c88ede6f0764f779500017f9a
anchalsinghrajput <83942897+anchalsinghrajput@users.noreply.github.com>
2021-06-01 10:38:02 +0530

    Create 4 area of triangle.c

---
+++ fast_track/4 area of triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{   
+    float base , height, area;
+      printf("\nEnter base and height of the triangle:");
+      scanf("%f  %f",&base,&height);
+      area=(base*height)/2 
+      printf("\n  area of the triangle with base %.1f and height %.1f is %.1f",                 base,height,area);
+
+    return 0;
+}
+
