74bc7844b8f379dc0ffac9f912e929dddcce680e
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2021-07-31 21:46:21 +0530

    Update 2 displaying elements of linked list.c

--- DSA/2 linked list/2 displaying elements of linked list.c
+++ DSA/2 linked list/2 displaying elements of linked list.c
@@ -145,6 +145,48 @@ int main()
     }
     
     return 0;
-   
+}
 
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+}*new,*head,*tail;
+
+int main()
+{
+    int value;
+    printf("Enter the elements in the node and enter -1 to terminate : ");
+    while(1)
+    {
+        scanf("%d",&value);
+        new = (struct node*)malloc (sizeof(struct node));
+   
+        new->data = value ;
+        new->next = NULL;
+        if(value==-1)
+        {
+            break;
+        }
+        else
+        {
+            if(head==NULL)
+                head=new;
+            else
+                tail->next=new;
+            tail=new;
+        }
+    }
+   
+    printf("Elements are : ");
+    struct node *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    
+    return 0;
 }
