99723e5effc24fdcc24adf1e7e3dc9faeb8e08ff
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2022-12-05 01:11:08 +0530

    Update os.c

--- os.c
+++ os.c
@@ -393,7 +393,7 @@ void main()
     for(int i =0; i<50; i++)
         index_block[i] = 0;
     
-    x : printf("\nEnter the index block : ");
+    x:printf("\nEnter the index block : ");
     scanf("%d",&idx);
     
     if(index_block[idx])
@@ -405,17 +405,30 @@ void main()
     printf("Enter the block needed : ");
     scanf("%d",&n);
     printf("Enter the number of files for the index %d on the disk : ",idx);
+    int count  = 0;
     for(int i = 0; i<n; i++)
+    {   
         scanf("%d",&index[i]);
-    
-    for(int i = 0; i<n; i++)
+        if(index_block[index[i]] == 0)
+        count++;
+    }
+        
+    if(count == n)
+    {
+        for(int i = 0; i<n; i++)
         index_block[index[i]] = 1;
         
-    printf("allocated");
-    printf("\nfile indexed");
-        
-    for(int i=0; i<n; i++)
-        printf("\n%d ------------ %d : %d",idx,index[i],index_block[index[i]]);
+        printf("allocated");
+        printf("\nfile indexed");
+            
+        for(int i=0; i<n; i++)
+            printf("\n%d ------------ %d : %d",idx,index[i],index_block[index[i]]);
+    }
+    else
+    {
+        printf("File in the index is already allocated");
+        goto x;
+    }
     
     int c;
 	printf("\nDo you want to enter more files(Yes - 1/No - 0):"); 
