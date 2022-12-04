af23370032df96449e4a62ec896771e7a52c569d
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2022-12-04 23:49:55 +0530

    Update os.c

--- os.c
+++ os.c
@@ -300,3 +300,89 @@ void main()
 
 // ================== program 7 ===============
 
+// ================== program 9 ===============
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char root_dir[10];
+    printf("Enter the root directory : ");
+    scanf("%s", root_dir);
+    
+    int n;
+    printf("Enter the number of files : ");
+    scanf("%d",&n);
+    
+    char sub_dir[n][10];
+    int count = 0;
+    
+    while(1)
+    {
+        int choice;
+        printf("do you want to create files? (1 - yes, any - no : ");
+        scanf("%d",&choice);
+        
+        if(choice !=1 )
+            break;
+        
+        char filename[10];
+        printf("Enter filename : ");
+        scanf("%s",filename);
+        
+        int found = 0;
+        for(int i = 0; i<n; i++)
+        {
+            if(strcmp(filename,sub_dir[i]) == 0)
+            {
+                found = 1;
+                break;
+            }
+        }
+        
+        if(found)
+        {
+                printf("File already exist! ");
+        }
+        else if(count == n)
+        {
+            printf("Number of file exceeded \n");
+        }
+        else
+        {
+            strcpy(sub_dir[count++],filename);
+        }
+        
+    }
+        printf("directory name: %s\n", root_dir);
+        printf("file names:\n");
+        for (int i = 0; i < n; i++)
+            printf("%s  ", sub_dir[i]);
+}
+
+output :
+Enter the root directory : anchal
+Enter the number of files : 5
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : one
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : two
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : three
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : four
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : five
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : d
+Number of file exceeded
+do you want to create files? (1 - yes, any - no) : 1
+Enter filename : r
+Number of file exceeded
+do you want to create files? (1 - yes, any - no) : 0
+directory name: anchal
+file names:
+one two three four five
+
+// ======================= program 10 ==================
+
