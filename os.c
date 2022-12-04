22786102189f4d8615723b0a38a4be170eac3db5
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2022-12-05 01:25:06 +0530

    Update os.c

--- os.c
+++ os.c
@@ -300,6 +300,72 @@ void main()
 
 // ================== program 7 ===============
 
+
+//=================program 8 ==================
+#include<stdio.h>
+int hitOrMiss(int num, int frames, int sf[])
+{
+    for(int i = 0; i<frames; i++)
+    {
+        if(num == sf[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int fcfs(int n, int frames, int a[], int sf[])
+{
+    int hit = 0, miss = 0, top = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(hitOrMiss(a[i],frames,sf))
+        {
+            hit++;
+        }
+        else
+        {
+            miss++;
+            sf[top%frames] = a[i];
+            top++;
+        }
+    }
+    return miss;
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of pages : ");
+    scanf("%d",&n);
+    
+    int frames;
+    printf("Enter number of frames : ");
+    scanf("%d",&frames);
+    
+    int a[n];
+    printf("Enter pages number : ");
+    for(int i = 0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    int sf[frames];
+    for(int i = 0; i<frames; i++)
+    {
+        sf[i] = -1;
+    }
+    printf("page fault : %d",fcfs(n,frames,a,sf));
+    
+}
+
+output:
+Enter the number of pages : 14
+Enter number of frames : 4
+Enter pages number : 7 0 1 2 0 3 0 4 2 3 0 3 2 3
+page fault : 7
+
 // ================== program 9 ===============
 #include<stdio.h>
 #include<string.h>
