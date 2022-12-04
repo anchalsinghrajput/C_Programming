64c057ca461eb93bbfbf8c95e952d2cb87d63f74
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2022-12-05 00:43:14 +0530

    Update os.c

--- os.c
+++ os.c
@@ -385,4 +385,43 @@ file names:
 one two three four five
 
 // ======================= program 10 ==================
-
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int index_block[50], idx, index[50],n;
+    for(int i =0; i<50; i++)
+        index_block[i] = 0;
+    
+    x : printf("\nEnter the index block : ");
+    scanf("%d",&idx);
+    
+    if(index_block[idx])
+    {
+        printf("\nindex is already allocated ");
+        goto x;
+    }
+  
+    printf("Enter the block needed : ");
+    scanf("%d",&n);
+    printf("Enter the number of files for the index %d on the disk : ",idx);
+    for(int i = 0; i<n; i++)
+        scanf("%d",&index[i]);
+    
+    for(int i = 0; i<n; i++)
+        index_block[index[i]] = 1;
+        
+    printf("allocated");
+    printf("\nfile indexed");
+        
+    for(int i=0; i<n; i++)
+        printf("\n%d ------------ %d : %d",idx,index[i],index_block[index[i]]);
+    
+    int c;
+	printf("\nDo you want to enter more files(Yes - 1/No - 0):"); 
+	scanf("%d", &c); 
+	if(c == 1) 
+		goto x; 
+	else
+		exit(0); 
+}
