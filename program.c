023224d2dbeb5b1809e36c8c246aa34fa2a3c7be
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2022-12-13 11:16:01 +0530

    Update program.c

--- program.c
+++ program.c
@@ -223,3 +223,156 @@ int main()
 	printf("\nTotal Time  : %f",totalTime);
 }
 
+#include<stdio.h>
+#include<string.h>
+char x[100000],y[100000],b[1000][1000];
+int i,j,m,n,a,c[1000][1000];
+void print_lcs(int i,int j)
+{
+	if(i==0 || j==0)
+		return ;
+	if(b[i][j]=='d')
+	{
+		print_lcs(i-1,j-1);
+		printf("%c",x[i-1]);
+	}
+	else if(b[i][j]=='u')
+	{
+		print_lcs(i-1,j);
+	}
+	else
+	{
+		print_lcs(i,j-1);
+	}
+}
+void lcs_length()
+{
+	m=strlen(x);
+	n=strlen(y);
+	for(i=0;i<=m;i++)
+		c[i][0]=0;
+	for(j=0;j<=n;j++)
+		c[0][j]=0;
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(x[i-1]==y[j-1])
+			{
+				c[i][j]=c[i-1][j-1]+1;
+				b[i][j]='d';     //diagonal arrow
+			}
+			else
+			{
+    			if(c[i-1][j]>=c[i][j-1])
+    			{
+    				c[i][j]=c[i-1][j];
+    				b[i][j]='u';      //upper arrow
+    			}
+    			else
+    			{
+    				c[i][j]=c[i][j-1];
+    				b[i][j]='s';       // side arrow
+    			}
+			}
+		}
+	}
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<=m;j++)
+		{
+			printf("%d  ",c[j][i]);
+		}
+		printf("\n");
+	}
+	print_lcs(m,n);
+}
+
+int main()
+{
+	printf("Welcome to the LCS Implementation in C");
+	printf("\nEnter 1st Sequence : ");
+	scanf("%s",x);
+	printf("\nEnter the 2nd Sequence : ");
+	scanf("%s",y);
+	printf("\nLongest Common Subsequence is:\n");
+	lcs_length();
+}
+
+
+// program 9 ================================ LCS ===============================
+#include<stdio.h>
+#include<string.h>
+char x[100000],y[100000],b[1000][1000];
+int i,j,m,n,a,c[1000][1000];
+void print_lcs(int i,int j)
+{
+	if(i==0 || j==0)
+		return ;
+	if(b[i][j]=='d')
+	{
+		print_lcs(i-1,j-1);
+		printf("%c",x[i-1]);
+	}
+	else if(b[i][j]=='u')
+	{
+		print_lcs(i-1,j);
+	}
+	else
+	{
+		print_lcs(i,j-1);
+	}
+}
+void lcs_length()
+{
+	m=strlen(x);
+	n=strlen(y);
+	for(i=0;i<=m;i++)
+		c[i][0]=0;
+	for(j=0;j<=n;j++)
+		c[0][j]=0;
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(x[i-1]==y[j-1])
+			{
+				c[i][j]=c[i-1][j-1]+1;
+				b[i][j]='d';     //diagonal arrow
+			}
+			else
+			{
+    			if(c[i-1][j]>=c[i][j-1])
+    			{
+    				c[i][j]=c[i-1][j];
+    				b[i][j]='u';      //upper arrow
+    			}
+    			else
+    			{
+    				c[i][j]=c[i][j-1];
+    				b[i][j]='s';       // side arrow
+    			}
+			}
+		}
+	}
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<=m;j++)
+		{
+			printf("%d  ",c[j][i]);
+		}
+		printf("\n");
+	}
+	print_lcs(m,n);
+}
+
+int main()
+{
+	printf("Welcome to the LCS Implementation in C");
+	printf("\nEnter 1st Sequence : ");
+	scanf("%s",x);
+	printf("\nEnter the 2nd Sequence : ");
+	scanf("%s",y);
+	printf("\nLongest Common Subsequence is:\n");
+	lcs_length();
+}
