5211ee835e26e9d4d8154510b22aacf72b1eabe1
Anchal Singh <83942897+anchalsinghrajput@users.noreply.github.com>
2022-12-14 11:37:15 +0530

    Update program.c

--- program.c
+++ program.c
@@ -223,6 +223,57 @@ int main()
 	printf("\nTotal Time  : %f",totalTime);
 }
 
+// program 5 ============================== prims ============================
+#include<stdio.h>
+int n,u,v,i,j,ne=1;
+int visited[10]={0},min,mincost=0,cost[10][10];
+void input()
+{
+    printf("\nEnter the adjacency matrix:\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            scanf("%d",&cost[i][j]);
+            if(cost[i][j]==0)
+                cost[i][j]=999;
+        }
+    }
+}
+
+void prims()
+{
+    visited[1]=1;
+    while(ne < n)
+    {
+        for(i=1,min=999;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(cost[i][j]< min && visited[i])
+                {
+                    min=cost[i][j];
+                    u=i;
+                    v=j;
+                }
+            }
+        }       
+        if(visited[u]==0 || visited[v]==0)
+        {
+            printf("\n Edge %d:(%d %d) cost:%d",ne++,u,v,min);
+            mincost+=min;
+            visited[v]=1;
+        }
+        cost[u][v]=cost[v][u]=999;
+    }
+    printf("\n Minimun cost=%d",mincost);
+}
+void main() {
+    printf("\nEnter the number of nodes:");
+    scanf("%d",&n);
+    input();
+    prims();
+}
 
 //program 6 ============================== floyd ==========================
 #include<stdio.h>
